a316e5d1d32ba2e887f55336c2e802109044d69e
losanguyen93 <nguyen2008@yandex.ru>
2016-06-01 13:51:59 +0300

    losa

--- app.js
+++ app.js
@@ -7,6 +7,14 @@
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
+var passport      = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+var bodyParser = require('body-parser');
+var multer     = require('multer'); 
+
+var cookieParser = require('cookie-parser');
+var session      = require('express-session');
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
@@ -16,13 +24,71 @@ var cfenv = require('cfenv');
 var app = express();
 
 // serve the files out of ./public as our main files
+app.use(cookieParser())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({ secret: 'this is the secret' }));
+app.use(multer());
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(__dirname + '/public'));
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
+
+passport.use(new LocalStrategy(
+function(username, password, done)
+{
+    if(username == 'admin' && password == 'alice')
+    {
+        var user = { firstName: 'Alice', lastName: 'Wonderland' };
+        return done(null, user);
+    }
+    return done(null, false, {message: 'Unable to login'});
+}));
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+
+var auth = function(req, res, next)
+{
+    if (!req.isAuthenticated())
+        res.send(401);
+    else
+        next();
+};
+
+app.get('/users', auth, function(req, res)
+{
+    res.json([{username: 123},{username: 234}]);
+});
+
+app.get('/loggedin', function(req, res)
+{
+    res.send(req.isAuthenticated() ? req.user : '0');
+});
+    
+app.post('/login', passport.authenticate('local'), function(req, res)
+{
+    res.send(req.user);
+});
+
+app.post('/logout', function(req, res)
+{
+    req.logOut();
+    res.send(200);
+});     
 
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
+var request_databaseDB_1 = require('./public/database')
+app.use('/database', request_databaseDB_1);
